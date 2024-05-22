36e8c00bec08a204a02be2e92b43a4c984aaab02
Joseph Mori Herrera <josephmoriherrera@MacBook-Air-de-Joseph.local>
2024-05-22 12:07:36 -0500

    app.blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="JOSSDEV, JD, BANQUEA.PE">
+        <meta name="description" content="Desarrollados de acuerdo a tus necesidades">
+
+        <title>JOSSDEV</title>
+        @vite('resources/css/app.css')
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css">
+
+    </head>
+    <body class="site-body min-w-96">
+        <header class="p-3 border-b bg-white">
+            <div class="container mx-auto flex justify-between items-center">
+                <img src="/img/logos/d-.png" alt="Logo de BANQUEA.pe" style="max-width: 200px; height: auto;">
+
+
+            </div>
+        </header>
+
+        <main class="bg-white mx-auto mt-2 text-black ">
+
+            <div>
+                <h2>
+                     @yield('titulo')
+                </h2>
+                    @yield('contenido')
+            </div>
+
+            
+        </main>
+
+        <!--FOOTER-->
+        <footer id="t3-footer" class="wrap t3-footer">
+            <div class="container">
+            <div class="t3-spotlight t3-footnav row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="t3-module module" id="Mod126">
+                    <div class="module-inner">
+                    <div class="module-ct">
+                        <div class="custom">
+                        <div class="footer-about">
+                            <img src="/img/logos/logo BM.png" alt="footer-logo" class="small-logo">
+                            <p>Buscamos la excelencia en todos nuestros desarrollos, nos adaptamos a tu presupuesto y
+                                 disfrutamos lo que hacemos.</p>
+                            <br>
+                            <ul class="footer-social">
+                            <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+                            <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+                            <li><a href="#"><i class="bi bi-whatsapp"></i></a></li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>       
+            </div>
+            </div>
+            <section class="t3-copyright">
+            <div class="wrap">
+                <div class="container">
+                <div class="custom">
+                    <div class="copyright">
+                    Copyright 2024 JOSSDEV, Todos los Derechos Reservados.
+                </div>
+                </div>
+            </div>
+            </section>
+        </foote>
+    </body>
+</html>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="keywords" content="JOSSDEV, JD, BANQUEA.PE">
-        <meta name="description" content="Desarrollados de acuerdo a tus necesidades">
-
-        <title>JOSSDEV</title>
-        @vite('resources/css/app.css')
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-    <body class="site-body min-w-96">
-       
-    </body>
-</html>
+@extends('layouts.app')
