a5c2b3f4a41067d0a1e855bfb61a2df2d43625d2
Joseph Mori Herrera <josephmoriherrera@MacBook-Air-de-Joseph.local>
2024-05-22 16:49:16 -0500

    app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,12 @@
         @vite('resources/css/app.css')
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap JS and dependencies -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet"
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,1 +1,26 @@
 @extends('layouts.app')
+
+@section('contenido')
+<div class="container mt-5">
+    <h2>Herramientas de Desarrollo</h2>
+    <div class="dev-tools-icons">
+        <img src="/img/tools/laravel.png" class="tool-icon" alt="Laravel">
+        <img src="/img/tools/flutter.png" class="tool-icon" alt="Flutter">
+        <img src="/img/tools/javascript.png" class="tool-icon" alt="JavaScript">
+        <img src="/img/tools/html.png" class="tool-icon" alt="html">
+    </div>
+</div>
+
+<div class="container mt-5">
+    <h2>Herramientas de Diseño</h2>
+    <div class="dev-tools-icons">
+        <img src="/img/tools/figma.svg.png" class="tool-icon" alt="Laravel">
+        <img src="/img/tools/ps.svg.png" class="tool-icon" alt="Flutter">
+        <img src="/img/tools/css.png" class="tool-icon" alt="css">
+    </div>
+</div>
+
+<br>
+
+
+@endsection
