0c3e449990438a6bd89c7d9f9bd2c5e459d75ec8
Branth95 <mori.bryan95@gmail.com>
2024-07-08 17:39:06 -0500

    Add navbar y banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,28 +10,40 @@
         @vite('resources/css/app.css')
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <!-- Bootstrap CSS -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-        <!-- Bootstrap JS and dependencies -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-        crossorigin="anonymous"
-      />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css">
 
     </head>
     <body class="site-body min-w-96">
-        <header class="p-3 border-b bg-white">
-            <div class="container mx-auto flex justify-between items-center">
-                <img src="/img/logos/d-.png" alt="Logo de BANQUEA.pe" style="max-width: 200px; height: auto;">
-
-
-            </div>
+        <header class="site-body min-w-96">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="/img/logos/d-.png" alt="Logo de BANQUEA.pe" style="max-width: 100px; height: auto;">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Portafolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sobre Nosotros</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
 
         <main class="bg-white mx-auto mt-2 text-black ">
@@ -75,6 +87,6 @@
                 </div>
             </div>
         </footer>
-        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 
 @section('contenido')
+<div id="main" class="">
+    <div>
+        <img src="/img/bg-software.jpg" class="d-block w-100" />
+    </div>
+    <div class="overlay">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 offset-md-6 text-center text-md-end">
+                    <h1>JOSSDEV</h1>
+                    <p class="d-none d-md-block">
+                    Buscamos la excelencia en todos nuestros desarrollos, nos adaptamos a tu presupuesto y disfrutamos lo que hacemos.
+                    </p>
+                    <a href="#" class="btn btn-outline-light">Deseo una WEB</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container mt-5">
     <h2>Herramientas de Desarrollo</h2>
     <div class="dev-tools-icons">
